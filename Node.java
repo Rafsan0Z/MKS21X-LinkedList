ef3fa5d6e75df46f559b5c11bcf8faa2e072d3f7
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-12-12 21:57:57 -0500

    Node constructor added!

--- Node.java
+++ Node.java
@@ -2,6 +2,12 @@ public class Node{
   private int data;
   private Node next,prev;
 
+  public Node(int value, Node n, Node p){
+    next = n;
+    prev = p;
+    data = value;
+  }
+
   public int get(){
     return data;
   }
